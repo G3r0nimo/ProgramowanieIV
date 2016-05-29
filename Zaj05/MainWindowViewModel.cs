76528965f39a358e3c9b75165c46f94fb52987e1
Damian <master0001@wp.pl>
2016-05-29 10:00:07 +0200

    zajęcia 29.05

---
+++ Zaj05/MainWindowViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zaj05
+{
+    public class MainWindowViewModel
+    {
+        public User DefaultUser { get; set; }
+        public List<User> AllUsers { get; set; }
+        public MainWindowViewModel()
+        {
+            AllUsers = new List<User>
+            {
+                new User {Login="Log1",Email="E1@ath.com" },
+                new User {Login="Log2",Email="E2@ath.com" },
+                new User {Login="Log3",Email="E3@ath.com" },
+
+            };
+            DefaultUser = AllUsers.FirstOrDefault();
+        }
+    }
+}
