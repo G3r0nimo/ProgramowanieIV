59ee06b90b640e1f5a64dd2d6ba6e7a5a3e9da71
Damian <master0001@wp.pl>
2016-06-05 09:32:57 +0200

    zajęcia06

---
+++ Zaj06/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zaj06.ViewModel
+{
+    public class MainWindowViewModel
+    {
+        public List<NoticeViewModel> Notices { get; set; }
+        public NoticeViewModel NoticeToAdd { get; set; }
+
+        public MainWindowViewModel()
+        {
+            //komunikacja z bazą
+
+            Notices = new List<NoticeViewModel>
+            {
+                new NoticeViewModel {Tittle="Zrobic projekt", Description="Na zaliczenie z C#", Created=DateTime.Now },
+                new NoticeViewModel {Tittle="Tittle 2", Description="Cos tam...", Created=DateTime.Now },
+                new NoticeViewModel {Tittle="Tittle 3", Description="Cos tam 123", Created=DateTime.Now },
+            };
+        }
+    }
+}
---
+++ Zaj06/ViewModel/NoticeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zaj06.ViewModel
+{
+    public class NoticeViewModel
+    {
+        public string Tittle { get; set; }
+        public DateTime Created { get; set; }
+        public string Description { get; set; }
+
+    }
+}
